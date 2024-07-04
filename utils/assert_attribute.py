6519bad20f7e2e0006c2b5d60483383f4d3916c0
Mohammad Bramantyo Putra Kusuma <mohammad.bramantyo@ui.ac.id>
2024-07-05 00:42:44 +0700

    add charity school test

--- utils/assert_attribute.py
+++ utils/assert_attribute.py
@@ -13,3 +13,22 @@ def assert_id_exists(driver):
     elements_id = driver.find_elements(By.CSS_SELECTOR, ".mb-2:nth-child(12) > div")
     assert len(elements_id) > 0, f"Assertion failed: ID does not exist"
     print(f"Assertion passed: ID exists, oject created successfully")
+    
+    
+def assert_id_disbursement(driver):
+    elements_id = driver.find_elements(By.CSS_SELECTOR, ".mb-2:nth-child(1) > div")
+    assert len(elements_id) > 0, f"Assertion failed: ID does not exist"
+    
+    id_content = elements_id[0].text.strip()
+    assert id_content, f"Assertion failed: ID does not exist"
+    print(f"Assertion passed: ID exists, oject created successfully")
+
+def assert_status_disbursement(driver):
+    status_element = driver.find_elements(By.CSS_SELECTOR, ".mb-2:nth-child(8) > div")
+    assert len(status_element) > 0, f"Tidak terdapat Status"
+    
+    status_content = status_element[0].text.strip()
+    assert status_content == "PENDING", f"Expected status to be 'PENDING' but got '{status_value}'"
+
+
+    print("Status is correctly set to 'PENDING'")
